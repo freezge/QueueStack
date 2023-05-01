7604ff5d00d7b8d59dd507df3647e180971b7ec4
freezge <freezge@gmail.com>
2023-05-01 22:28:15 +0600

    mylinkedliststack new methods

--- untitled/src/MyLinkedListStack.java
+++ untitled/src/MyLinkedListStack.java
@@ -1,2 +1,17 @@
 public class MyLinkedListStack {
+    MyLinkedListStack(){}
+    private MyLinkedList stack = new MyLinkedList();
+    public void push(Object o){}
+    public Object pop(){
+        return null;
+    }
+    public Object peek(){
+        return null;
+    }
+    public boolean isEmpty(){
+        return true;
+    }
+    public int size(){
+        return 0;
+    }
 }
