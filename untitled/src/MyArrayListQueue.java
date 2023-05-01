5b6602a301b5fe93b258f07872ca7511e2fcafb9
freezge <freezge@gmail.com>
2023-05-01 22:03:43 +0600

    new methods for myarrlistq

--- untitled/src/MyArrayListQueue.java
+++ untitled/src/MyArrayListQueue.java
@@ -1,2 +1,17 @@
 public class MyArrayListQueue {
+    MyArrayListQueue(){}
+    private MyArrayList queue = new MyArrayList();
+    public void enqueue(Object o){}
+    public Object dequeue(Object o){
+        return null;
+    }
+    public Object peek(){
+        return null;
+    }
+    public boolean isEmpty(){
+        return true;
+    }
+    public int size(){
+        return 0;
+    }
 }
