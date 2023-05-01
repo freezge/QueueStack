a5d60304a7c9ab1851734af2a9ac3fafb2f79e5b
freezge <freezge@gmail.com>
2023-05-01 22:05:30 +0600

    myarrlistqpeek

--- untitled/src/MyArrayListQueue.java
+++ untitled/src/MyArrayListQueue.java
@@ -1,12 +1,14 @@
 public class MyArrayListQueue {
     MyArrayListQueue(){}
     private MyArrayList queue = new MyArrayList();
-    public void enqueue(Object o){}
+    public void enqueue(Object o){
+
+    }
     public Object dequeue(Object o){
         return null;
     }
     public Object peek(){
-        return null;
+        return queue.get(0);
     }
     public boolean isEmpty(){
         return true;
