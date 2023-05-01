59591164ab4f5c05a0bae13bd6df08a6fd3a85b5
freezge <freezge@gmail.com>
2023-05-01 22:20:44 +0600

    myarrliststack new methods

--- untitled/src/MyArrayListStack.java
+++ untitled/src/MyArrayListStack.java
@@ -1,2 +1,17 @@
 public class MyArrayListStack {
+    MyArrayListStack(){}
+    private MyArrayList stack = new MyArrayList();
+    public void push(Object o){}
+    public Object pop(){
+        return null;
+    }
+    public Object peek(){
+        return null;
+    }
+    public boolean isEmpty(){
+        return true;
+    }
+    public int size(){
+        return 0;
+    }
 }
