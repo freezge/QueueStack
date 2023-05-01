3094b64a75afa67b1e64862cc39ac6a06eeb5e4f
freezge <freezge@gmail.com>
2023-05-01 22:29:33 +0600

    mylinkedliststack isempty method

--- untitled/src/MyLinkedListStack.java
+++ untitled/src/MyLinkedListStack.java
@@ -13,7 +13,7 @@ public class MyLinkedListStack {
         return stack.get(stack.size() - 1);
     }
     public boolean isEmpty(){
-        return true;
+        return stack.size() == 0;
     }
     public int size(){
         return 0;
