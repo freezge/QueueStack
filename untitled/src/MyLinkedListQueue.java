f30330021afe38fd33e94e1f39a6cf9fcaef7b79
freezge <freezge@gmail.com>
2023-05-01 22:35:34 +0600

    mylinkedlistq comments

--- untitled/src/MyLinkedListQueue.java
+++ untitled/src/MyLinkedListQueue.java
@@ -1,22 +1,22 @@
 public class MyLinkedListQueue {
-    MyLinkedListQueue(){}
-    private MyLinkedList queue = new MyLinkedList();
-    public void enqueue(Object o){
+    MyLinkedListQueue(){}//constructor for MyLinkedListQueue
+    private MyLinkedList queue = new MyLinkedList();//create an instance of LinkedList
+    public void enqueue(Object o){//adds new element to the end of queue
         queue.add(o);
     }
-    public Object dequeue(){
-        Object buff = queue.get(0);
-        queue.remove(0);
-        return buff;
+    public Object dequeue(){//removes the front element of the queue
+        Object buff = queue.get(0);//to save element
+        queue.remove(0);//removing front element
+        return buff;//returns removed element
     }
-    public Object peek(){
-        return queue.get(0);
+    public Object peek(){//to get front elem of q
+        return queue.get(0);//returns front elem of q
     }
-    public boolean isEmpty(){
-        return queue.size() == 0;
+    public boolean isEmpty(){//to check if q is empty
+        return queue.size() == 0;//if size = 0 q is empty
     }
-    public int size(){
-        return queue.size();
+    public int size(){//method to get size of q
+        return queue.size();//returns size of q
     }
 
 }
