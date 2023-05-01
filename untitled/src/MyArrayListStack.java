70d4f2890d40319b38b0defd29aea815326815a8
freezge <freezge@gmail.com>
2023-05-01 22:38:15 +0600

    myarrliststack comments

--- untitled/src/MyArrayListStack.java
+++ untitled/src/MyArrayListStack.java
@@ -1,21 +1,21 @@
 public class MyArrayListStack {
-    MyArrayListStack(){}
-    private MyArrayList stack = new MyArrayList();
-    public void push(Object o){
+    MyArrayListStack(){}//constructor for MyArrayListStack
+    private MyArrayList stack = new MyArrayList();//create an instance of ArrayList
+    public void push(Object o){//adds new element to the end of stack
         stack.add(o);
     }
-    public Object pop(){
-        Object buff = stack.get(stack.size() - 1);
-        stack.remove(stack.size() - 1);
-        return buff;
+    public Object pop(){//removes the top element of the stack
+        Object buff = stack.get(stack.size() - 1);//to save element
+        stack.remove(stack.size() - 1);//removing top element
+        return buff;//returns removed element
     }
-    public Object peek(){
-        return stack.get(stack.size() - 1);
+    public Object peek(){//to get top elem of stack
+        return stack.get(stack.size() - 1);//returns top elem of stack
     }
-    public boolean isEmpty(){
-        return stack.size() == 0;
+    public boolean isEmpty(){//to check if stack is empty
+        return stack.size() == 0;//if size = 0 stack is empty
     }
-    public int size(){
-        return stack.size();
+    public int size(){//method to get size of stack
+        return stack.size();//returns size of stack
     }
 }
