a749decacf212339cd0491fbf75d9a2f53f09ba5
freezge <freezge@gmail.com>
2023-05-01 22:19:02 +0600

    mylinkedlistq fix

--- untitled/src/MyLinkedListQueue.java
+++ untitled/src/MyLinkedListQueue.java
@@ -1,6 +1,6 @@
 public class MyLinkedListQueue {
     MyLinkedListQueue(){}
-    private static MyLinkedList queue = new MyLinkedList();
+    private MyLinkedList queue = new MyLinkedList();
     public void enqueue(Object o){
         queue.add(o);
     }
